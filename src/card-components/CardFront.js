f796dada1ecdda77fdb73218d91fb77128b74986
dstewart <davidstewart@gmail.com>
2019-11-22 19:30:34 -0700

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 export default class CardFront extends Component {
-
+  
   render() {
+    {debugger}
     return (
+    
       <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
+      
       </div>
     )
   }
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -34,7 +34,7 @@ export default class MovieCard extends Component {
       <div className="movie-card">
         { 
           /* which component should receive which props? */}
-        <CardFront url={posterMap[this.props.poster]} />
+        <CardFront poster={posterMap[this.props.poster]} />
         <CardBack 
           title={this.props.title}
           IMDBRating={this.props.IMDBRating}
